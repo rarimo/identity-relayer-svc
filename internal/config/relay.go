dfbd6df7a5541d12e82a208ca796040e7053ff60
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-08-21 14:33:37 +0300

    fixed relay config

--- internal/config/relay.go
+++ internal/config/relay.go
@@ -3,13 +3,11 @@ package config
 import (
 	"gitlab.com/distributed_lab/figure/v3"
 	"gitlab.com/distributed_lab/kit/kv"
-	"gitlab.com/distributed_lab/logan/v3"
 	"gitlab.com/distributed_lab/logan/v3/errors"
 )
 
 type RelayConfig struct {
-	TargetChain     string `fig:"target_chain,required"`
-	CatchupDisabled bool   `fig:"catchup_disabled"`
+	CatchupDisabled bool `fig:"catchup_disabled"`
 }
 
 func (c *config) Relay() RelayConfig {
@@ -25,13 +23,6 @@ func (c *config) Relay() RelayConfig {
 			Please()
 		if err != nil {
 			panic(errors.Wrap(err, "failed to figure out "+yamlName))
-		}
-
-		if _, ok := c.EVM().GetChainByName(cfg.TargetChain); !ok {
-			panic(errors.From(errors.New("target chain not found"), logan.F{
-				"chain":        cfg.TargetChain,
-				"config_entry": yamlName,
-			}))
 		}
 
 		return cfg
